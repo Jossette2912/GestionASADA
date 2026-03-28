fd3c42e796675aa00a3dede5bf77178fe5509266
David Cespedes Carranza <dcespedesc@edu.uc.ac.cr>
2026-03-28 13:09:11 -0600

    Se agregan archivos medidor

---
+++ GestionASADA/DataBase/MedidorDB.vb
@@ -0,0 +1,44 @@
+﻿Imports GestionASADA.Models
+Imports GestionASADA.Utils
+Imports System.Data.SqlClient
+
+
+'DBHelper Se utiliza para manejar la conexión a la base de datos y ejecutar consultas SQL. 
+'La clase MedidorDB contiene métodos para interactuar con la tabla de medidores en la base de datos, como crear un nuevo medidor.
+
+
+Public Class MedidorDB
+    Private db As New DbHelper()
+    'crear medidor
+
+    Public Function CrearMedidor(ByVal pMedidor As Models.Medidor, ByRef errorMessage As String) As Boolean
+        'logica para crear un nuevo medidor en la base de datos
+
+        Using db.GetConnection()
+            Dim query As String = "
+            INSERT INTO medidor (Suscriptor, Medidor, Ubicacion, Estado) 
+            VALUES (@Suscriptor, @Medidor, @Ubicacion, @Estado)"
+
+            Dim parameters As New Dictionary(Of String, Object) From {
+             {"@Suscriptor", pMedidor.Suscriptor},
+             {"@Medidor", pMedidor.Medidor},
+            {"@Ubicacion", pMedidor.Ubicacion},
+            {"@Estado", pMedidor.Estado}
+            }
+
+            Return db.ExecuteNonQuery(query, parameters, errorMessage)
+
+        End Using
+        Return True
+    End Function
+
+    Public Function eliminarMedidor(ByVal Medidor As Integer, ByRef errorMessage As String) As Boolean
+
+        Dim query As String = "DELETE FROM medidor WHERE Medidor = @Medidor"
+        Dim parameters As New Dictionary(Of String, Object) From {
+                {"@Medidor", Medidor}
+            }
+        Return db.ExecuteNonQuery(query, parameters, errorMessage)
+    End Function
+
+End Class
---
+++ GestionASADA/Models/Medidor.vb
@@ -0,0 +1,69 @@
+﻿Namespace Models
+
+    Public Class Medidor
+
+        Private _suscriptor As String
+        Private _medidor As Integer
+        Private _ubicacion As String
+        Private _estado As String
+
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(medidor As Integer, suscriptor As String, ubicacion As String, estado As String)
+
+            _suscriptor = suscriptor
+            _medidor = medidor
+            _ubicacion = ubicacion
+            _estado = estado
+
+        End Sub
+
+        Public Property Suscriptor As String
+            Get
+                Return _suscriptor
+            End Get
+            Set(value As String)
+                _suscriptor = value
+            End Set
+        End Property
+
+        Public Property Medidor As Integer
+            Get
+                Return _medidor
+            End Get
+            Set(value As Integer)
+                _medidor = value
+            End Set
+        End Property
+
+        Public Property Ubicacion As String
+            Get
+                Return _ubicacion
+            End Get
+            Set(value As String)
+                _ubicacion = value
+            End Set
+        End Property
+
+        Public Property Estado As String
+            Get
+                Return _estado
+            End Get
+            Set(value As String)
+                _estado = value
+            End Set
+        End Property
+
+
+        Public Function Resumen() As String
+            Return $"Medidor: {Suscriptor} -  {Medidor} - {Ubicacion} - {Estado}"
+        End Function
+
+    End Class
+
+
+
+
+End Namespace
