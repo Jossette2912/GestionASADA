80fff207ed1c8d43a41c246e83368ecec55c30ae
David Cespedes Carranza <dcespedesc@edu.uc.ac.cr>
2026-03-28 14:16:40 -0600

    Se agrega archivos Medidor

---
+++ GestionASADA/Medidor.aspx
@@ -0,0 +1,149 @@
+﻿<%@ Page Title="Gestion de Medidor" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Medidor.aspx.vb" Inherits="Gestion_ASADA.Medidor" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+ <%--          https://github.com/DavidCc04/Gestion-ASADA/commits/master/                 --%>
+
+    <%--*************************   ID suscriptor    *******************************--%>
+
+    <div class="form-group">
+        <asp:Label ID="lblsuscriptor" runat="server" Text="ID de Suscriptor:" CssClass="control-label"></asp:Label>
+        <asp:TextBox ID="txtSuscriptor" runat="server" placeholder="001" CssClass="form-control"></asp:TextBox>
+    </div>
+
+    <%--* Validacion suscriptor *--%>
+    <asp:RequiredFieldValidator ID="rfvsuscriptor" runat="server"
+        ErrorMessage="Es necesario indicar el ID de suscriptor."
+        ControlToValidate="txtSuscriptor" Display="Dynamic" CssClass="text-danger">
+    </asp:RequiredFieldValidator>
+
+
+    <%--*************************   ID Medidor    *******************************--%>
+    
+    <div class="form-group">
+        <asp:Label ID="lblMedidor" runat="server" Text="ID de Medidor" CssClass="control-label"></asp:Label>
+        <asp:TextBox ID="txtMedidor" runat="server" placeholder="0001" CssClass="form-control"></asp:TextBox>
+    </div>
+
+    <%--* Validacion medidor *--%>
+    <asp:RequiredFieldValidator ID="rfvmedidor" runat="server"
+        ErrorMessage="Es necesario indicar el ID de Medidor."
+        ControlToValidate="txtMedidor" Display="Dynamic" CssClass="text-danger">
+    </asp:RequiredFieldValidator>
+    
+
+    <%--*************************   Ubucacion   *******************************--%>
+  
+    <div class="form-group">
+        <asp:Label ID="lblUbicacion" runat="server" Text="Ubicacion" CssClass="control-label"></asp:Label>
+        <asp:TextBox ID="txtUbicacion" runat="server" placeholder="San Francisco" CssClass="form-control"></asp:TextBox>
+        <%--<asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>--%>
+       
+        <%--* Validacion Ubicacion *--%>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server"
+            ErrorMessage="Debe indicar la ubicacion del medidor."
+            ControlToValidate="txtubicacion" Display="Dynamic" ForeColor="Red">  
+        </asp:RequiredFieldValidator> 
+    </div>
+
+
+    <%--*************************   Estado   *******************************--%>
+  
+    <div class="form-group">
+        <asp:Label ID="lblEstadoActual" runat="server" Text="Estado Actual" CssClass="control-label"></asp:Label>
+        <asp:DropDownList ID="ddlEstadoActual" runat="server" CssClass="form-control">
+            <asp:ListItem Text="Seleccione el estado actual" Value="" />
+            <asp:ListItem Text="ACTIVO" Value="1"></asp:ListItem>
+            <asp:ListItem Text="INACTIVO" Value="2" />
+            <asp:ListItem Text="MOROSO" Value="3" />
+        </asp:DropDownList>
+    </div>
+
+    <%--* Validacion Estado *--%>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server"
+        ErrorMessage="Es necesario indicar el estado del medidor."
+        ControlToValidate="ddlEstadoActual" InitialValue=""  Display="Dynamic"  CssClass="text-danger"> 
+    </asp:RequiredFieldValidator>
+    <br />
+    <br />
+
+    <asp:Label ID="lblResultado" runat="server" Text="Resultado:" CssClass="control-label"></asp:Label>
+
+    <%--Agrega una linea en blanco     <hr class="my-3" />                  --%>
+    <%--<br />                                          Espacio sin linea      --%>
+    <hr style="border-top: 2px solid #333;" />
+    <%--  Agrega una linea Gruesa--%>
+
+
+
+    <%--  ********************************    Solo botones ******************************************** --%>
+
+
+    <asp:Button ID="btnConsultar" runat="server" Text="Consultar" CssClass="btn btn-success my-2" OnClick="btnConsultar_Click" />
+    <asp:Button ID="btnInsertar" runat="server" Text="Insertar" CssClass="btn btn-success" OnClick="btnInsertar_Click" />
+    <asp:Button ID="btnModificar" runat="server" Text="Modificar" CssClass="btn btn-success" />
+    <asp:Button ID="btnLimpiar" runat="server" Text="Limpiar" CssClass="btn btn-success" />
+    <asp:Button ID="Eliminar" runat="server" Text="Eliminar" CssClass="btn btn-success" />
+
+
+    <%--  *******************************    cuadro para los botones   ************************************** --%>
+
+
+
+   
+
+
+
+    <%-- <div class="card p-4" style="background-color:#8fa2bd;">
+            
+            <div class="row mb-3">
+                <div class="col-6">
+                    <asp:Button ID="Button1" runat="server" Text="CONSULTAR" CssClass="btn btn-light w-100" />
+                </div>
+                <div class="col-6">
+                    <asp:Button ID="Button2" runat="server" Text="LIMPIAR" CssClass="btn btn-light w-100" />
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-6">
+                    <asp:Button ID="Button3" runat="server" Text="MODIFICAR" CssClass="btn btn-light w-100" />
+                </div>
+                <div class="col-6">
+                    <asp:Button ID="btnEliminar" runat="server" Text="ELIMINAR" CssClass="btn btn-light w-100" />
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-6">
+                    <asp:Button ID="Button4" runat="server" Text="INSERTAR" CssClass="btn btn-light w-100" />
+                </div>
+                <div class="col-6">
+                    <asp:Button ID="btnAtras" runat="server" Text="ATRAS" CssClass="btn btn-success w-100" />
+                </div>
+            </div>--%>
+
+
+   <%-- ********************************************************************************************************************** --%>
+
+  
+    
+    <asp:GridView ID="gvMedidores" runat="server" AutoGenerateColumns="False" DataKeyNames="Suscriptor" DataSourceID="SqlDataSource1" OnRowDeleting="gvMedidores_RowDeleting">
+        <Columns>
+            <asp:BoundField DataField="Suscriptor" HeaderText="Suscriptor" InsertVisible="False" ReadOnly="True" SortExpression="Suscriptor" />
+            <asp:BoundField DataField="Medidor" HeaderText="Medidor" SortExpression="Medidor" />
+            <asp:BoundField DataField="Ubicacion" HeaderText="Ubicacion" SortExpression="Ubicacion" />
+            <asp:BoundField DataField="Estado" HeaderText="Estado" SortExpression="Estado" />
+            <asp:CommandField showDeleteButton="true" />
+
+        </Columns>
+    </asp:GridView>
+
+
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:GestionAsadaConnectionString %>" ProviderName="<%$ ConnectionStrings:GestionAsadaConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [Medidor]"></asp:SqlDataSource>
+
+
+</asp:Content>
+
+
---
+++ GestionASADA/Medidor.aspx.designer.vb
@@ -0,0 +1,197 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+Namespace Gestion_ASADA
+
+    Partial Public Class Medidor
+
+        '''<summary>
+        '''Control lblsuscriptor.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents lblsuscriptor As Global.System.Web.UI.WebControls.Label
+
+        '''<summary>
+        '''Control txtSuscriptor.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents txtSuscriptor As Global.System.Web.UI.WebControls.TextBox
+
+        '''<summary>
+        '''Control rfvsuscriptor.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents rfvsuscriptor As Global.System.Web.UI.WebControls.RequiredFieldValidator
+
+        '''<summary>
+        '''Control lblMedidor.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents lblMedidor As Global.System.Web.UI.WebControls.Label
+
+        '''<summary>
+        '''Control txtMedidor.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents txtMedidor As Global.System.Web.UI.WebControls.TextBox
+
+        '''<summary>
+        '''Control rfvmedidor.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents rfvmedidor As Global.System.Web.UI.WebControls.RequiredFieldValidator
+
+        '''<summary>
+        '''Control lblUbicacion.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents lblUbicacion As Global.System.Web.UI.WebControls.Label
+
+        '''<summary>
+        '''Control txtUbicacion.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents txtUbicacion As Global.System.Web.UI.WebControls.TextBox
+
+        '''<summary>
+        '''Control RequiredFieldValidator1.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents RequiredFieldValidator1 As Global.System.Web.UI.WebControls.RequiredFieldValidator
+
+        '''<summary>
+        '''Control lblEstadoActual.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents lblEstadoActual As Global.System.Web.UI.WebControls.Label
+
+        '''<summary>
+        '''Control ddlEstadoActual.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents ddlEstadoActual As Global.System.Web.UI.WebControls.DropDownList
+
+        '''<summary>
+        '''Control RequiredFieldValidator2.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents RequiredFieldValidator2 As Global.System.Web.UI.WebControls.RequiredFieldValidator
+
+        '''<summary>
+        '''Control lblResultado.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents lblResultado As Global.System.Web.UI.WebControls.Label
+
+        '''<summary>
+        '''Control btnConsultar.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents btnConsultar As Global.System.Web.UI.WebControls.Button
+
+        '''<summary>
+        '''Control btnInsertar.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents btnInsertar As Global.System.Web.UI.WebControls.Button
+
+        '''<summary>
+        '''Control btnModificar.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents btnModificar As Global.System.Web.UI.WebControls.Button
+
+        '''<summary>
+        '''Control btnLimpiar.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents btnLimpiar As Global.System.Web.UI.WebControls.Button
+
+        '''<summary>
+        '''Control Eliminar.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents Eliminar As Global.System.Web.UI.WebControls.Button
+
+        '''<summary>
+        '''Control gvMedidores.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents gvMedidores As Global.System.Web.UI.WebControls.GridView
+
+        '''<summary>
+        '''Control SqlDataSource1.
+        '''</summary>
+        '''<remarks>
+        '''Campo generado automáticamente.
+        '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        '''</remarks>
+        Protected WithEvents SqlDataSource1 As Global.System.Web.UI.WebControls.SqlDataSource
+    End Class
+End Namespace
